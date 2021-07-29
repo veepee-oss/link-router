8270cfe33e8544bef35433d54e12d5b5977d6380
Julio Cesar Bueno Cotta <julio.buenocotta@veepee.com>
2021-07-29 11:01:51 +0200

    adds license to all kotlin files

--- library/src/main/java/com/veepee/vpcore/route/link/deeplink/InvalidDeepLinkParameterException.kt
+++
@@ -1,4 +0,0 @@
-package com.veepee.vpcore.route.link.deeplink
-
-class InvalidDeepLinkParameterException(deepLink: DeepLink, message: String = "") :
-    IllegalArgumentException("$deepLink lacks parameters. Message: $message")
